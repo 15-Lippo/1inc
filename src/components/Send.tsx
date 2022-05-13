d82ea172367a0bc67f5b5779d1380f7ddaab3755
Andrii Leonets <awdenheimlee@gmail.com>
2022-05-13 10:25:06 +0300

    field for input amount with debounce (#29)
    * feat: implement Send component
    
    * feat: add check for presence of token in input field
    
    * fix: fix Send component
    
    * fix: add check for debounceTypedValueSwap

---
+++ src/components/Send.tsx
@@ -0,0 +1,40 @@
+import { parseUnits } from '@ethersproject/units';
+import { TextField } from '@mui/material';
+import _ from 'lodash';
+import React from 'react';
+
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { Field, typeInput } from '../store/state/swap/swapSlice';
+
+export interface SendProps {
+  inputId: Field;
+}
+
+export default function Send({ inputId }: SendProps) {
+  const dispatch = useAppDispatch();
+  const { INPUT, OUTPUT } = useAppSelector((state) => state.swap);
+
+  const setTypedValueSwap = (value: string, field: Field) => {
+    const decimals = field === Field.INPUT ? INPUT.currency.decimals : OUTPUT.currency.decimals;
+    const valueInWei = parseUnits(value, decimals).toString();
+    dispatch(typeInput({ field, typedValue: valueInWei }));
+  };
+
+  const debounceTypedValueSwap = _.debounce(setTypedValueSwap, 1000);
+
+  const handleChange = ({ target }: any) => {
+    if (INPUT.currency.address) debounceTypedValueSwap(target.value, target.id);
+  };
+
+  return (
+    <TextField
+      inputProps={{
+        min: 0,
+      }}
+      id={inputId}
+      type="number"
+      defaultValue={0}
+      onChange={handleChange}
+    />
+  );
+}
