7593488eee5b36b7712f3adeafca58037585b630
Andrii Leonets <awdenheimlee@gmail.com>
2022-09-01 15:02:32 +0300

    feat: add tests

--- src/test/defaultValues.test.tsx
+++ src/test/defaultValues.test.tsx
@@ -1,15 +1,49 @@
-import { screen, waitFor } from '@testing-library/react';
+import { parseUnits } from '@ethersproject/units';
+import { render, waitFor } from '@testing-library/react';
 import React from 'react';
-import { act } from 'react-dom/test-utils';
 
 import { SwapWidget } from '../index';
-import { renderWidget } from '.';
 
 describe('Default values', () => {
   it('defaultInputTokenAddress set', async () => {
-    act(() => {
-      renderWidget(<SwapWidget />);
-    });
-    await waitFor(() => expect(screen.findByTestId('input-token')).toBeInTheDocument());
+    const component = render(
+      <SwapWidget
+        defaultInputTokenAddress={{
+          1: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+        }}
+      />
+    );
+
+    const input = await component.findByTestId('input-token');
+
+    await waitFor(() => expect(input).toHaveTextContent('USDC'));
+  });
+
+  it('defaultOutputTokenAddress set', async () => {
+    const component = render(
+      <SwapWidget
+        defaultOutputTokenAddress={{
+          1: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+        }}
+      />
+    );
+
+    const output = await component.findByTestId('output-token');
+
+    await waitFor(() => expect(output).toHaveTextContent('ETH'));
+  });
+
+  it('defaultTypedValue set', async () => {
+    const component = render(
+      <SwapWidget
+        defaultTypedValue={{
+          1: parseUnits('2', 6).toString(),
+        }}
+      />
+    );
+
+    const value = await component.findByDisplayValue('2.0');
+
+    await waitFor(() => expect(value).toHaveDisplayValue('2.0'));
   });
 });
