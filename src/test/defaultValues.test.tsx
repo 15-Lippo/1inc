6db110fa0f7e45844b8b72421509850079e6242a
Andrii Leonets <awdenheimlee@gmail.com>
2022-09-14 12:13:34 +0300

    feat: update tests

--- src/test/defaultValues.test.tsx
+++ src/test/defaultValues.test.tsx
@@ -1,48 +1,55 @@
 import { parseUnits } from '@ethersproject/units';
-import { render, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 
 import { SwapWidget } from '../index.prod';
 
 describe('Default values', () => {
   it('defaultInputTokenAddress set', async () => {
-    const component = render(
-      <SwapWidget
-        defaultInputTokenAddress={{
-          1: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-        }}
-      />
-    );
+    act(() => {
+      render(
+        <SwapWidget
+          defaultInputTokenAddress={{
+            1: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+          }}
+        />
+      );
+    });
 
-    const input = await component.findByTestId('input-token');
+    const input = await screen.findByTestId('input-token');
 
     await waitFor(() => expect(input).toHaveTextContent('USDC'));
   });
 
   it('defaultOutputTokenAddress set', async () => {
-    const component = render(
-      <SwapWidget
-        defaultOutputTokenAddress={{
-          1: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
-        }}
-      />
-    );
+    act(() => {
+      render(
+        <SwapWidget
+          defaultOutputTokenAddress={{
+            1: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+          }}
+        />
+      );
+    });
 
-    const output = await component.findByTestId('output-token');
+    const output = await screen.findByTestId('output-token');
 
     await waitFor(() => expect(output).toHaveTextContent('ETH'));
   });
 
   it('defaultTypedValue set', async () => {
-    const component = render(
-      <SwapWidget
-        defaultTypedValue={{
-          1: parseUnits('2', 6).toString(),
-        }}
-      />
-    );
+    act(() => {
+      render(
+        <SwapWidget
+          defaultTypedValue={{
+            1: parseUnits('2', 6).toString(),
+          }}
+        />
+      );
+    });
 
-    const value = await component.findByDisplayValue('2.0');
+    const value = await screen.findByDisplayValue('2.0');
 
     await waitFor(() => expect(value).toHaveDisplayValue('2.0'));
   });
